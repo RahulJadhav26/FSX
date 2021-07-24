6ebc0057aea7245ca7616e9e8b132d7b9a667338
RahulJadhav2611 <rrahuljadhav.rj@gmail.com>
2021-07-24 15:55:55 -0400

    Error handling and validation of CRUD

--- routes/dynamodbAPI.js
+++ routes/dynamodbAPI.js
@@ -16,17 +16,38 @@ router.post("/create",(req,res)=>{
         Item: obj
     }
     console.log("Adding a new item...");
-
-    docClient.put(params, function(err, data) {
+    var paramscheck = {
+        TableName:table,
+        Key: {
+            fileSystemId:obj.fileSystemId
+        }
+    }
+    docClient.get(paramscheck, function(err, data) {
         if (err) {
+            console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
             res.send(err)
-            console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
         } else {
-            console.log("Added item:", JSON.stringify(data, null, 2));
-            res.send(data)
+            console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+            console.log(data)
+            if(data.Item) {
+                console.log("Item Already present")
+                res.send({status: false, msg:"Item already Present"})
+            } else {
+                docClient.put(params, function(err, data) {
+                    if (err) {
+                        res.send(err)
+                        console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+                    } else {
+                        console.log("Added item:", JSON.stringify(data, null, 2));
+                        res.send({status: true, msg:"Item Added successfully"})
+                    }
+                });
+            }
         }
     });
+
 })
+
 router.post("/delete",(req,res)=>{
     var obj = req.body
     var params = {
@@ -35,19 +56,30 @@ router.post("/delete",(req,res)=>{
     }
     console.log("Deleting a new item...");
 
-    docClient.delete(params, function(err, data) {
+    docClient.get(params, function(err, data) {
         if (err) {
-            console.error("Unable to delete item. Error JSON:", JSON.stringify(err, null, 2));
+            console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
             res.send(err)
         } else {
-            console.log("DeleteItem succeeded:", JSON.stringify(data, null, 2));
-            res.send(data)
+            console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+            if(data.Item){
+                docClient.delete(params, function(err, data) {
+                    if (err) {
+                        console.error("Unable to delete item. Error JSON:", JSON.stringify(err, null, 2));
+                        res.send(err)
+                    } else {
+                        console.log("DeleteItem succeeded:", JSON.stringify(data, null, 2));
+                        res.send({status: true, msg:"Item Deleted Successfully"})
+                    }
+                });
+            } else{
+                res.send({status:false,msg:"Item not found, No Delete operation done"})
+            }
         }
     });
-})
-module.exports = router
 
-router.get('/list', (req,res)=>{
+})
+router.post('/list', (req,res)=>{
     var obj = req.body
     params = {
         TableName: table,
@@ -60,7 +92,13 @@ router.get('/list', (req,res)=>{
             res.send(err)
         } else {
             console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
-            res.send(data)
+            if(data.Item){
+                res.send({status:true, data:data})
+            }else{
+                res.send({status:false,msg:"No Items Found"})
+            }
         }
     });
 })
+
+module.exports = router
